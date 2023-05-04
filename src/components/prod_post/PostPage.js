190f0e26487bcf6a340e681ff951876e03946729
s-woon <sus9763@gmail.com>
2023-05-04 14:52:17 +0900

    판매목록 페이지 구현

---
+++ src/components/prod_post/PostPage.js
@@ -0,0 +1,251 @@
+import React, { useEffect, useState } from "react";
+import styled, { css } from "styled-components";
+import ProdPost from "./ProdPost";
+import productlist from "./prod.json";
+import filter from "./filter.json";
+import axios from "axios";
+
+const PostPage = () => {
+  const memberId = "admin";
+  const [prodData, setProdData] = useState();
+  // 경매, 즉시구매 저장 변수 all, auction, direct, none
+  const [selectedSellType, setSelectedSellType] = useState("all");
+  // 카테고리 저장 변수
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [selectedSort, setSelectedSort] = useState("view");
+
+  // 경매, 즉시구매 토글 기능을 위한 상태 변수
+  const [auctionState, setAuctionState] = useState(true);
+  const [directState, setDirectState] = useState(true);
+
+  // 가전, 가구가 true 일때 세부 카테고리 렌더링을 위한 상태 변수
+  const [applianceState, setApplianceState] = useState(false);
+  const [furnitureState, setFurnitureState] = useState(false);
+
+  // return <ul className="listWrap">{list}</ul>;
+
+  // 경매, 즉시구매 누르면 토글기능
+  const sellTypeHandler = (type) => {
+    if(type === "auction") {
+      setAuctionState(!auctionState);
+    } else if (type === "direct") {
+      setDirectState(!directState);
+    }
+  };
+
+  // 카테고리
+  const categoryHandler = (category) => {
+    setSelectedCategory(category);
+    if(category === "all") {
+      setApplianceState(false);
+      setFurnitureState(false);
+    } else if(category.includes("app")) {
+      setApplianceState(true);
+      setFurnitureState(false);
+    } else if (category.includes("fur")) {
+      setApplianceState(false);
+      setFurnitureState(true);
+    }
+  }
+
+  // 판매목록 데이터 받아오기
+  const getProdData = () => {
+    console.log(selectedSellType);
+    console.log(selectedCategory);
+    // axios.get(`/api/getproducts?selltype=${selectedSellType}&category=${selectedCategory}`)
+    // .then((res) => {
+    //   const { data } = res;
+    //   setProdData(data);
+    // })
+    // .catch((e) => {
+    //   console.error(e);
+    // })
+    const data = productlist.prodlist; 
+    setProdData(data);
+  };
+
+  useEffect(() => {
+    // 경매, 즉시구매 버튼 누를 때마다 axios로 담아줄 selectedSellType 변수 값 변경
+    if((auctionState === true) & (directState === true)) {
+      setSelectedSellType("all");
+    } else if((auctionState === true) & (directState === false)) {
+      setSelectedSellType("auction");
+    } else if((directState === true) & (auctionState === false)) {
+      setSelectedSellType("direct");
+    } else {
+      setSelectedSellType("none");
+    }
+  }, [auctionState, directState])
+
+  useEffect(() => {
+    // 필터조건이 바뀔때마다 데이터에 axios 요청
+    getProdData();
+  }, [selectedSellType, selectedCategory]);
+
+  return (
+    <>
+      <FilterBox>
+        <TopFilterBox isAdmin={memberId==="admin"}>
+          <BuyFilterBox>
+            <SellTypeSpan active={auctionState} onClick={()=>{sellTypeHandler("auction")}}>경매</SellTypeSpan>
+            <SellTypeSpan active={directState} onClick={()=>{sellTypeHandler("direct")}}>즉시구매</SellTypeSpan>
+          </BuyFilterBox>
+          <StateFilterBox>
+            <span><label><input type="checkbox" /> 진행예정</label></span>
+            <span><label><input type="checkbox" /> 진행중</label></span>
+            <span><label><input type="checkbox" /> 종료</label></span>
+          </StateFilterBox>
+        </TopFilterBox>
+        <BottomFilterBox>
+          <CategoryFilterBox>
+            <CategorySpan active={!applianceState & !furnitureState} onClick={()=>{categoryHandler("all")}}>전체</CategorySpan>
+            <CategorySpan active={applianceState} onClick={()=>{categoryHandler("appliance")}}>가전</CategorySpan>
+            <CategorySpan active={furnitureState} onClick={()=>{categoryHandler("furniture")}}>가구</CategorySpan>
+            {applianceState && (
+              <>
+                <CategoryDetailSpan active={selectedCategory === "appliance"} onClick={()=>{categoryHandler("appliance")}}>가전 전체</CategoryDetailSpan>
+                <CategoryDetailSpan active={selectedCategory === "appkiechen"} onClick={()=>{categoryHandler("appkiechen")}}>주방</CategoryDetailSpan>
+                <CategoryDetailSpan active={selectedCategory === "applife"} onClick={()=>{categoryHandler("applife")}}>생활</CategoryDetailSpan>
+                <CategoryDetailSpan active={selectedCategory === "appelec"} onClick={()=>{categoryHandler("appelec")}}>전자기기</CategoryDetailSpan>
+              </>
+            )}
+            {furnitureState && (
+              <>
+                <CategoryDetailSpan active={selectedCategory === "furniture"} onClick={()=>{categoryHandler("furniture")}}>가구 전체</CategoryDetailSpan>
+                <CategoryDetailSpan active={selectedCategory === "furliving"} onClick={()=>{categoryHandler("furliving")}}>거실/주방</CategoryDetailSpan>
+                <CategoryDetailSpan active={selectedCategory === "furbed"} onClick={()=>{categoryHandler("furbed")}}>침실</CategoryDetailSpan>
+                <CategoryDetailSpan active={selectedCategory === "furoffice"} onClick={()=>{categoryHandler("furoffice")}}>사무실</CategoryDetailSpan>
+              </>
+            )}
+          </CategoryFilterBox>
+          <OrderbyFilterBox>
+              <select>
+                <option value="">최신순</option>
+                <option value="">조회순</option>
+                <option value="">마감순</option>
+              </select>
+          </OrderbyFilterBox>
+        </BottomFilterBox>
+      </FilterBox>
+      <ProdListBox>
+        <ProdPost products={prodData} />
+      </ProdListBox>
+    </>
+  );
+};
+export default PostPage;
+
+// 필터박스
+const FilterBox = styled.div`
+  width: 1400px;
+  height: 82px;
+  /* background-color: aliceblue; */
+  margin: 73px auto 50px;
+
+  font-family: 'Noto Sans';
+  font-style: normal;
+`;
+
+const TopFilterBox = styled.div`
+  display: flex;
+  /* display: ${({isAdmin}) => isAdmin ? "block": "flex"}; */
+  justify-content: space-between;
+  border-bottom: 1px solid #514438;
+  height: 41px;
+  line-height: 41px;
+`;
+
+const BuyFilterBox = styled.div`
+  margin-left: 30px;
+  font-weight: 700;
+  font-size: 25px;
+  color: #514438;
+  span:first-child {
+    margin-right: 30px;
+  }
+`;
+
+const SellTypeSpan = styled.span`
+  ${({ active }) => active ? css`color: #514438;` : css`color: rgba(81, 68, 56, 0.6);`};
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const StateFilterBox = styled.div`
+  margin-right: 35px;
+  font-size: 20px;
+  font-weight: 600;
+  color: rgba(81, 68, 56, 0.8);
+  span {
+    margin-right: 20px;
+  }
+  span:last-child {
+    margin-right: 0px;
+  }
+  span > label:hover {
+    cursor: pointer;
+  }
+`;
+
+const BottomFilterBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 41px;
+  line-height: 41px;
+`;
+
+const CategoryFilterBox = styled.div`
+  margin-left: 30px;
+  font-weight: 700;
+  font-size: 25px;
+  color: rgba(81, 68, 56, 0.6);
+`;
+
+const CategorySpan = styled.span`
+  ${({ active }) => active ? css`color: #514438;` : css`color: rgba(81, 68, 56, 0.6);`};
+  margin-right: 30px;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const CategoryDetailSpan = styled.span`
+  ${({ active }) => active ? css`color: #514438;` : css`color: rgba(81, 68, 56, 0.6);`};
+  margin-right: 30px;
+  font-weight: 500;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const OrderbyFilterBox = styled.div`
+  select {
+    width: 84px;
+    height: 25px;
+    line-height: 25px;
+    margin-right: 35px;
+
+    border: 0px;
+    // FIXME: select 화살표 커스텀
+    /* appearance: none; */ 
+    /* -webkit-appearance: none; chrome */
+    /* -moz-appearance: none; firefox   */
+    /* background: url("./arrow.png") no-repeat 97% 50%/15px auto; */
+
+    font-size: 20px;
+    font-weight:500;
+    color: rgba(81, 68, 56, 0.8);
+
+    :hover {
+      cursor: pointer;
+    }
+  }
+`;
+
+
+// 상품리스트 박스
+const ProdListBox = styled.div`
+  width: 1350px;
+  margin: 0px auto;
+`;
---
+++ src/components/prod_post/ProdPost.js
@@ -0,0 +1,445 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const ProdPost = ({ products }) => {
+  // TODO: staricon만 클릭
+  const navigate = useNavigate();
+  const [today, setToday] = useState(new Date().getTime()); // 현재날짜(ms) 구하기
+
+  const lessThanTwelve = (ms) => {
+    const diff = ms - today;
+    const day = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    if (day === 0) {
+      return hours < 12;
+    } else {
+      return false;
+    }
+  };
+
+  // 남은시간(ms) 계산 후 string로 반환
+  const getDdayString = (destination_date_ms) => {
+    const diff = destination_date_ms - today;
+
+    const day = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+    const ddayString = `${day === 0 ? `` : `${day}일`} 
+                        ${hours === 0 ? `` : `${hours}시간`} 
+                        ${minutes === 0 ? `` : `${minutes}분`} 
+                        ${seconds === 0 ? `` : `${seconds}초`}`;
+    return ddayString;
+  };
+
+  // 시작일이 현재시간보다 크면 start_date(시작일), 작으면 end_date(종료일)로 남은시간 받아오기
+  const getDday = ({ start_date, end_date }) => {
+    if (Date.parse(start_date) > today) {
+      return getDdayString(Date.parse(start_date));
+    } else {
+      return getDdayString(Date.parse(end_date));
+    }
+  };
+
+  // 찜버튼
+  const likeHandler = (event, board_num) => {
+    event.stopPropagation(); // 이벤트 버블링 막기
+    const data = { board_num: board_num, id: "회원정보" };
+    console.log(data);
+    // axios.post("/api/likehandle", data)
+    // .then((res) => {
+    //   console.log(res);
+    // })
+    // .catch((e) => {
+    //   console.error(e);
+    // });
+  };
+
+  // 조회수 올리는 axios 요청 후 성공하면 상세페이지 넘어가기
+  const prodDetailHandler = (board_num) => {
+    console.log("dd");
+    navigate(`/post/detail/${board_num}`);
+    // const data = {board_num: board_num};
+    // axios.post("/api/plusreadcount", data)
+    // .then((res) => {
+    //   // console.log(res);
+    //   navigate(`/post/detail/${board_num}`);  
+    // })
+    // .catch((e) => {
+    //   console.error(e);
+    // });
+  };
+
+  // 1초마다 리렌더링
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setToday(new Date().getTime());
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [today]);
+
+  return (
+    <>
+      {products?.map((data) => (
+        <ProductBox
+          key={data.board_num}
+          onClick={() => prodDetailHandler(data.board_num)}
+        >
+          <ImageBox>
+            {/* <img src={data.main_image} alt="productimage" /> */}
+            <img src={`/images/prod/${data.main_image}`} alt="productimage" />
+
+            {/* 경매, 즉시구매 글자 렌더링 */}
+            {data.sell_type === 1 && <Auction>경매</Auction>}
+            {data.sell_type === 2 && <DirectBuy>즉시구매</DirectBuy>}
+            {data.sell_type === 3 && (
+              <>
+                <DirectAndAuction>
+                  <div>경매</div>
+                  <div>즉시구매</div>
+                </DirectAndAuction>
+              </>
+            )}
+
+            {/* 찜 유무 */}
+            {data.like === 0 ? (
+              <StarIcon onClick={(event) => likeHandler(event, data.board_num)}>
+                <img src="images/prod/star_icon.png" alt="staricon" />
+              </StarIcon>
+            ) : (
+              <StarIcon onClick={(event) => likeHandler(event, data.board_num)}>
+                <img src="images/prod/star_icon_filled.png" alt="staricon" />
+              </StarIcon>
+            )}
+
+            {/* 판매완료 상품 어둡게 */}
+            {data.prod_state === 2 && <DarkCover />}
+            {/* 판매완료(상품현황1이지만 경매종료일 지난) 상품 */}
+            {(data.prod_state === 1) & (Date.parse(data.end_date) < today) ? (
+              <DarkCover />
+            ) : null}
+            {/* 오픈예정 상품 어둡게 */}
+            {Date.parse(data.start_date) > today && (
+              <DarkCover>
+                <span>경매 오픈 예정</span>
+              </DarkCover>
+            )}
+          </ImageBox>
+          <InfoBox>
+            {/* 경매상품이거나 경매, 즉시구매이고 오픈예정이 아니고 경매종료일이 지나지 않은 상품 */}
+            {(((data.sell_type === 1) & (data.prod_state === 1)) |
+              ((data.sell_type === 3) & (data.prod_state === 1))) &
+            (Date.parse(data.start_date) < today) &
+            (Date.parse(data.end_date) > today) &
+            (lessThanTwelve(Date.parse(data.end_date)) !== true) ? (
+              <TimeBox>
+                <img src="images/prod/time_icon.png" alt="timeicon" />
+                {getDday(data)}
+              </TimeBox>
+            ) : null}
+            {/* 12시간 미만 상품 빨간색으로 남은시간 표시 */}
+            {(((data.sell_type === 1) & (data.prod_state === 1)) |
+              ((data.sell_type === 3) & (data.prod_state === 1))) &
+            (Date.parse(data.start_date) < today) &
+            (Date.parse(data.end_date) > today) &
+            (lessThanTwelve(Date.parse(data.end_date)) === true) ? (
+              <RedTimeBox>
+                <img src="images/prod/time_icon_red.png" alt="timeicon" />
+                {getDday(data)}
+              </RedTimeBox>
+            ) : null}
+
+            {/* 경매오픈예정 상품 */}
+            {Date.parse(data.start_date) > today && (
+              <TimeBox>
+                <img src="images/prod/time_icon.png" alt="timeicon" />
+                {getDday(data)}
+              </TimeBox>
+            )}
+
+            {/* 즉시구매, 판매중 상품 */}
+            {(data.sell_type === 2) & (data.prod_state === 1) ? (
+              <TimeBox>─</TimeBox>
+            ) : null}
+
+            {/* 판매완료(상품현황: 2) 상품 */}
+            {data.prod_state === 2 && (
+              <TimeBox>
+                <img src="images/prod/time_icon.png" alt="timeicon" />
+                판매종료
+              </TimeBox>
+            )}
+            {/* 판매완료(상품현황1이지만 경매종료일 지난) 상품 */}
+            {(data.prod_state === 1) & (Date.parse(data.end_date) < today) ? (
+              <TimeBox>
+                <img src="images/prod/time_icon.png" alt="timeicon" />
+                판매종료
+              </TimeBox>
+            ) : null}
+
+            <PrdoInfoBox>
+              <ProdComBox>{data.prod_com}</ProdComBox>
+              <ProdNameBox>{data.prod_name}</ProdNameBox>
+
+              {/* bid_count가 null이 아닐경우에만 경매참여자 렌더링 */}
+              {data.bid_count !== null && (
+                <BidCountBox>경매참여자 : {data.bid_count}명</BidCountBox>
+              )}
+
+              {/* 경매 */}
+              {data.sell_type === 1 && (
+                <OriAndNowPriceBox>
+                  <span>{data.org_price.toLocaleString("ko-KR")}원</span>
+                  {data.cur_price.toLocaleString("ko-KR")}원
+                </OriAndNowPriceBox>
+              )}
+
+              {/* 즉시구매 */}
+              {data.sell_type === 2 && (
+                <OriAndNowPriceBox>
+                  <span>{data.org_price.toLocaleString("ko-KR")}원</span>
+                  {data.direct_price.toLocaleString("ko-KR")}원
+                </OriAndNowPriceBox>
+              )}
+
+              {/* 경매, 즉시구매 */}
+              {data.sell_type === 3 && (
+                <>
+                  <OriAndNowPriceBox>
+                    <span>{data.org_price.toLocaleString("ko-KR")}원</span>
+                    {data.cur_price.toLocaleString("ko-KR")}원
+                  </OriAndNowPriceBox>
+                  <DirectPriceBox>
+                    <span>즉시구매가</span>
+                    {data.direct_price.toLocaleString("ko-KR")}원
+                  </DirectPriceBox>
+                </>
+              )}
+            </PrdoInfoBox>
+          </InfoBox>
+        </ProductBox>
+      ))}
+    </>
+  );
+};
+
+export default ProdPost;
+
+const ProductBox = styled.div`
+  width: 300px;
+  height: 525px;
+  margin: 0px 50px 50px 0px;
+  display: inline-block;
+  vertical-align: middle;
+
+  font-family: "Noto Sans";
+  font-style: normal;
+  :nth-child(4n) {
+    margin-right: 0px;
+  }
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const ImageBox = styled.div`
+  width: 300px;
+  height: 300px;
+
+  position: relative;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const StarIcon = styled.div`
+  width: 30px;
+  height: 30px;
+
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+
+  z-index: 1;
+`;
+
+const DarkCover = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  position: absolute;
+  top: 0px;
+  left: 0px;
+
+  span {
+    height: 30px;
+    color: rgba(255, 255, 255, 0.6);
+
+    font-size: 25px;
+    line-height: 30px;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin: -15px 0 0 -82px;
+  }
+`;
+
+const Auction = styled.div`
+  width: 50px;
+  height: 30px;
+  text-align: center;
+  line-height: 30px;
+  background-color: #b2d4c1;
+  border-radius: 5px;
+
+  position: absolute;
+  top: 10px;
+  right: 10px;
+
+  font-family: "Noto Sans";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+`;
+
+const DirectBuy = styled.div`
+  width: 79px;
+  height: 30px;
+  text-align: center;
+  line-height: 30px;
+  background-color: #b2d4c1;
+  border-radius: 5px;
+
+  position: absolute;
+  top: 10px;
+  right: 10px;
+
+  font-family: "Noto Sans";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+`;
+
+const DirectAndAuction = styled.div`
+  font-family: "Noto Sans";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+
+  div:first-child {
+    width: 50px;
+    height: 30px;
+    text-align: center;
+    line-height: 30px;
+    background-color: #b2d4c1;
+    border-radius: 5px;
+
+    position: absolute;
+    top: 10px;
+    right: 99px;
+  }
+  div:last-child {
+    width: 79px;
+    height: 30px;
+    text-align: center;
+    line-height: 30px;
+    background-color: #b2d4c1;
+    border-radius: 5px;
+
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+`;
+
+const InfoBox = styled.div`
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const TimeBox = styled.div`
+  font-size: 18px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #b9a89a;
+  margin: 3px 0px 0px 0px;
+  img {
+    width: 18px;
+    height: 18px;
+    margin: 2px 3px 0px 0px;
+  }
+`;
+
+const RedTimeBox = styled.div`
+  font-size: 18px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ff0000;
+  margin: 3px 0px 0px 0px;
+  img {
+    width: 18px;
+    height: 18px;
+    margin: 2px 3px 0px 0px;
+  }
+`;
+
+const PrdoInfoBox = styled.div`
+  margin: 0px 5px 0px 5px;
+`;
+
+const ProdComBox = styled.div`
+  margin-top: 10px;
+
+  font-size: 18px;
+  font-weight: 400;
+  color: #777777;
+`;
+
+const ProdNameBox = styled.div`
+  margin: 5px 0px 5px 0px;
+  font-size: 25px;
+  font-weight: 400;
+`;
+
+const BidCountBox = styled.div`
+  margin: 5px 0px 0px 0px;
+  text-align: right;
+  font-size: 18px;
+  font-weight: 400;
+  color: #888888;
+`;
+
+const OriAndNowPriceBox = styled.div`
+  text-align: right;
+  font-size: 25px;
+  font-weight: 600;
+  span {
+    font-weight: 400;
+    font-size: 18px;
+    color: #333333;
+    text-decoration: line-through;
+    margin-right: 12px;
+  }
+`;
+
+const DirectPriceBox = styled.div`
+  text-align: right;
+  font-size: 25px;
+  font-weight: 600;
+  span {
+    font-weight: 400;
+    font-size: 18px;
+    color: #333333;
+    margin-right: 10px;
+  }
+`;
