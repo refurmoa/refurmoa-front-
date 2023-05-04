fa12dd0158a17f9e7eb8d8ccf4bcb52e73bdf998
wstarY25 <127380218+wstarY25@users.noreply.github.com>
2023-05-04 15:13:55 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import { Route, Routes } from "react-router-dom";
-import PostPage from "./components/prodPost/PostPage";
-import MyPage from "./components/myPage/MyPage";
-
 function App() {
   return (
     <>
