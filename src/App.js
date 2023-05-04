84f802e23eac7a5067abcc38733b0284506c99cc
s-woon <sus9763@gmail.com>
2023-05-04 16:38:38 +0900

    route 통합

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import PostPage from "./components/prodPost/PostPage";
+import MyPage from "./components/myPage/MyPage";
+import Header from "./components/headerNfooter/Header";
+import Login from "./components/sign/signin/Login";
+import SignupMain from "./components/sign/signup/SignupMain";
+import SignupPhone from "./components/sign/signup/SignupPhone";
+import SignupInput from "./components/sign/signup/SignupInput";
+import SignupAccept from "./components/sign/signup/SignupAccept";
+import SignupComplete from "./components/sign/signup/SignupComplete";
+import NoticeView from "./components/cs/NoticeView";
+import NoticeMain from "./components/cs/NoticeMain";
+
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/post" element={<PostPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/signup" element={<SignupMain />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup/2" element={<SignupPhone />} />
+        <Route path="/signup/3" element={<SignupInput />} />
+        <Route path="/signup/4" element={<SignupAccept />} />
+        <Route path="/signup/5" element={<SignupComplete />} />
+        <Route path="/notice" element={<NoticeMain />} />
+        <Route path="/notice/detail/:noticeid" element={<NoticeView />} />
+      </Routes>
     </>
   )
 
