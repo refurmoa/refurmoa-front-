2660e78665b7ebb2a7e7eca249091d128be1a1f6
Sejin <w_star25@naver.com>
2023-05-10 15:08:14 +0900

    Feat: 타이머, 모달창 등 기능 추가

--- src/components/prodPost/PostDetail/PostDetail.js
+++ src/components/prodPost/PostDetail/PostDetail.js
@@ -1,8 +1,9 @@
 // 판매 상세 페이지
 
 import "./PostDetail.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
+import { getDdayArray } from "../../shared/sharedFn";
 import PostProdDetail from "./PostProdDetail";
 import ProdInquiry from "./ProdInquiry";
 import SellInfo from "./SellInfo";
@@ -17,23 +18,26 @@ function PostDetail() {
   const board_num = useParams().board_num;
   const navigate = useNavigate();
   // const login_id = window.sessionStorage.getItem("member_id"); // 세션 ID
-  const [prodInfo, setProdInfo] = useState(post);
-  const [bidList, setBidList] = useState(bidlist);
-  const [state, setState] = useState(0);
-  const [starState, setStarState] = useState(0);
-  const [bidPrice, setBidPrice] = useState(0);
-  const [countBid, setCountBid] = useState(0);
-  const [bidListState, setBidListState] = useState(0);
-  const [detailInfo, setDetailInfo] = useState(1);
+  const [prodInfo, setProdInfo] = useState(post); // 상품 상세 정보
+  const [bidList, setBidList] = useState(bidlist); // 입찰 내역
+  const [state, setState] = useState(0); // 판매 상태 (0 : 판매예정, 1 : 판매중, 2 : 판매종료)
+  const [starState, setStarState] = useState(false); // 찜 상태
+  const [bidPrice, setBidPrice] = useState(prodInfo.cur_price + prodInfo.unit_price); // 입찰가
+  const [countBid, setCountBid] = useState(0); // 입찰 수
+  const [bidListState, setBidListState] = useState(false); // 입찰내역 열기/닫기
+  const [imageView, setImageView] = useState(0); // 하자 이미지 모달 창
+  const [detailInfo, setDetailInfo] = useState(1); // 메뉴 (1 : 상세정보, 2 : 상품문의, 3 : 결제 및 배송, 4 : 교환환불)
+  const [now, setNow] = useState(new Date().getTime()); // 현재 날짜(ms)
+
   const login_id = "admin";
 
   // sell_type => 1 : 경매, 2 : 즉시구매, 3 : 경매+즉시구매
 
   useEffect (() => {
-    // 상품 정보
+    // 상품 정보 조회
     // setProdInfo();
 
-    // 판매 상태(state) (0 : 판매예정, 1 : 판매중, 2 : 판매종료)
+    // 판매 상태
     const start = new Date(prodInfo.start_date);
     const end = new Date(prodInfo.end_date);
     const today = new Date();
@@ -44,21 +48,22 @@ function PostDetail() {
     // 찜 상태
     login_id !== null && prodStar();
 
-    // 입찰가
-    setBidPrice(prodInfo.cur_price + prodInfo.unit_price);
-
     // 입찰 내역 조회
     // setBidList();
 
     // 입찰 수 조회
     prodInfo.sell_type !== 2 && setCountBid(7);
+
+    // 타이머 > 1초마다 리렌더링
+    const countdown = setInterval(() => {
+      setNow(new Date().getTime());
+      getDday();
+    }, 1000);
+
+    return () => {
+      clearInterval(countdown);
+    };
   }, []);
-
-  // 타이머 시작
-  
-
-
-  // 타이머 끝
 
   // 삭제
   const postDelete = () => {
@@ -70,7 +75,7 @@ function PostDetail() {
 
   // 찜 조회 (0 : 찜X, 1 : 찜O)
   const prodStar = () => {
-    // setStarState(1);
+    // setStarState(true);
   };
 
   // 찜 등록/취소
@@ -78,7 +83,7 @@ function PostDetail() {
     if (login_id === null) {
       navigate("/login");
     } else {
-      starState === 0 ? setStarState(1) : setStarState(0);
+      !starState ? setStarState(true) : setStarState(false);
     }
   };
 
@@ -87,12 +92,42 @@ function PostDetail() {
     // content === "grade" ?  : content === "delivery" ?  : ;
   }
 
+  // 타이머
+  const getDday = () => {
+    const end_date = new Date(prodInfo.end_date);
+    const {day, hours, minutes, seconds} = getDdayArray(Date.parse(end_date));
+    const red = day === 0 && hours < 12 ? true : false;
+    return (
+      <li className="PD-price_info_text">
+        <span>남은시간</span>
+        { day !== 0 && <>
+          <span className={red ? "PD-timer_num_red" : "PD-timer_num"}>{day}</span>
+          <span className={red ? "PD-timer_text_red" : "PD-timer_text"}>일&nbsp;</span>
+          </>
+        }
+        { hours !== 0 && <>
+          <span className={red ? "PD-timer_num_red" : "PD-timer_num"}> {hours}</span>
+          <span className={red ? "PD-timer_text_red" : "PD-timer_text"}>시간&nbsp;</span>
+          </>
+        }
+        { minutes !== 0 && <>
+          <span className={red ? "PD-timer_num_red" : "PD-timer_num"}> {minutes}</span>
+          <span className={red ? "PD-timer_text_red" : "PD-timer_text"}>분&nbsp;</span>
+          </>
+        }
+        { seconds !== 0 && <>
+        <span className={red ? "PD-timer_num_red" : "PD-timer_num"}> {seconds}</span>
+        <span className={red ? "PD-timer_text_red" : "PD-timer_text"}>초&nbsp;</span>
+        </>
+      }
+      </li>
+    );
+  };
+
   // 입찰가 설정 (minus / plus)
   const bidPriceUpdate = (c) => {
-    // let updatePrice = bidPrice;
-    // c === "minus" ? updatePrice = bidPrice - prodInfo.unit_price
-    // : updatePrice = bidPrice + prodInfo.unit_price
-    // setBidPrice(0);
+    c === "minus" ? setBidPrice(bidPrice - prodInfo.unit_price)
+    : setBidPrice(bidPrice + prodInfo.unit_price);
   }
 
   // 입찰
@@ -117,9 +152,9 @@ function PostDetail() {
 
   // 입찰내역 열기/닫기
   const bidListOut = () => {
-      bidListState === 1 ? setBidListState(0)
+      bidListState ? setBidListState(false)
       : login_id === null ? alert("로그인 후 확인 가능합니다.")
-      : setBidListState(1);
+      : setBidListState(true);
   }
 
   // 입찰내역
@@ -139,8 +174,12 @@ function PostDetail() {
   ));
 
   // 이미지 모달 창
-  const imageView = () => {
-
+  const imageViewChange = (i) => {
+    if (imageView !== 0) setImageView(0);
+    else if (i === 1) setImageView(1);
+    else if (i === 2) prodInfo.deffect_image2 !== null && setImageView(2);
+    else if (i === 3) prodInfo.deffect_image3 !== null && setImageView(3);
+    else setImageView(0);
   }
 
   return (
@@ -153,13 +192,14 @@ function PostDetail() {
           <span className={prodInfo.sell_type === 1 ? "PD-selltype_none" : (state === 1 ? "PD-selltype_circle" : "PD-selltype_circle_end")}></span>
           <span className={prodInfo.sell_type === 1 ? "PD-selltype_none" : "PD-selltype_text"}>즉시구매</span>
         </span>
-        {login_id === "admin" && (
-          <span className="PD_update_delete">
-            <Link to={`/post/update/${board_num}`}>수정</Link>
-            <span>|</span>
+        {login_id === "admin" && 
+          <span className="PD-update_delete">
+            { state === 0 &&
+              <Link to={`/post/update/${board_num}`}>수정</Link>
+            }
             <span onClick={postDelete}>삭제</span>
           </span>
-        )}
+        }
       </div>
 
       <div className="PD-info_wrap">
@@ -167,7 +207,7 @@ function PostDetail() {
           <span className="PD-main_image_wrap">
             <div className="PD-star_position">
             <img className="PD-main_image" alt="상품이미지" src={`/images/${prodInfo.main_image}`}></img>
-              { starState === 0 ?
+              { !starState ?
                 <img className="PD-star" alt="찜하기" src={star_icon_line} onClick={starClick}></img>
                 : <img className="PD-star" alt="찜하기" src={star_icon_filled} onClick={starClick}></img>
               }
@@ -227,7 +267,7 @@ function PostDetail() {
               </li>
               <li className="PD-main_info_text">
                 <span>배송설치비</span>
-                <span>{prodInfo.delivery_price === 0 ? `무료` : `${prodInfo.delivery_price.toLocaleString('ko-KR')}원`}</span>
+                <span>{prodInfo.delivery_price === 0 ? "무료" : `${prodInfo.delivery_price.toLocaleString('ko-KR')}원`}</span>
                 <img className="PD-main_info_icon" alt="배송설치비 상세정보" src={info_icon_brown} onClick={InfoIconClick("delivery")}></img>
               </li>
             </ul>
@@ -245,17 +285,7 @@ function PostDetail() {
             <hr className="PD-info_line"></hr>
             { prodInfo.sell_type !== 2 && state === 1 && <>
               <ul className="PD-price_info_wrap">
-                <li className="PD-price_info_text">
-                  <span>남은시간</span>
-                  <span className="PD-timer_num">00</span>
-                  <span className="PD-timer_text">일&nbsp;</span>
-                  <span className="PD-timer_num"> 00</span>
-                  <span className="PD-timer_text">시간&nbsp;</span>
-                  <span className="PD-timer_num"> 00</span>
-                  <span className="PD-timer_text">분&nbsp;</span>
-                  <span className="PD-timer_num"> 00</span>
-                  <span className="PD-timer_text">초</span>
-                </li>
+                {getDday()}
                 <li className="PD-price_info_text">
                   <span>현재가</span>
                   <span className="PD-current_price_num">{prodInfo.cur_price.toLocaleString('ko-KR')}</span>
@@ -267,19 +297,34 @@ function PostDetail() {
                     <img className="PD-bid_info_icon" alt="입찰방법" src={info_icon_brown} onClick={InfoIconClick("bid_info")}></img>
                   </span>
                   <span className="PD-bid_price_wrap">
-                    <span className="PD-bid_price_0" onClick={bidPriceUpdate("minus")}>&lt;</span>
-                    <span className="PD-bid_price_1">{bidPrice - prodInfo.unit_price}</span>
+                    { bidPrice === prodInfo.cur_price + prodInfo.unit_price ? 
+                      <span className="PD-bid_price_0_gray">&lt;</span>
+                      : <button className="PD-bid_price_0" onClick={() => {bidPriceUpdate("minus")}}>&lt;</button>
+                    }
+                    <span className={bidPrice === prodInfo.cur_price + prodInfo.unit_price ? "PD-bid_price_none" : "PD-bid_price_1"}>
+                      {bidPrice - prodInfo.unit_price}
+                    </span>
                     <span className="PD-bid_price_2">{bidPrice} <span>원</span></span>
-                    <span className="PD-bid_price_3">{bidPrice + prodInfo.unit_price}</span>
-                    <span className="PD-bid_price_4">{bidPrice + prodInfo.unit_price * 2}</span>
-                    <span className="PD-bid_price_0" onClick={bidPriceUpdate("plus")}>&gt;</span>
+                    <span className={prodInfo.direct_price !== null && bidPrice === prodInfo.direct_price ? "PD-bid_price_none" : "PD-bid_price_3"}>
+                      {bidPrice + prodInfo.unit_price}
+                    </span>
+                    <span className={prodInfo.direct_price !== null && bidPrice >= prodInfo.direct_price - prodInfo.unit_price ? "PD-bid_price_none" : "PD-bid_price_4"}>
+                      {bidPrice + prodInfo.unit_price * 2}
+                    </span>
+                    { prodInfo.direct_price !== null && bidPrice === prodInfo.direct_price ? 
+                      <span className="PD-bid_price_0_gray">&gt;</span>
+                      : <button className="PD-bid_price_0" onClick={() => {bidPriceUpdate("plus")}}>&gt;</button>
+                    }
                   </span>
                 </li>
               </ul>
 
               {/* 구매/입찰 버튼 */}
               <div className="PD-buy_btn_wrap">
-                <button className="PD-buy_btn_filled" type="button" onClick={bidBuy}>입&nbsp;&nbsp;&nbsp;찰</button>
+                { bidPrice !== prodInfo.direct_price &&
+                  <button className="PD-buy_btn_filled" type="button" onClick={bidBuy}>입&nbsp;&nbsp;&nbsp;찰</button>
+                }
+                
                 { prodInfo.sell_type === 3 &&
                   <button className="PD-buy_btn_line" type="button" onClick={directBuy}>
                     {prodInfo.direct_price.toLocaleString('ko-KR')}<span>원에</span> 즉시 구매<span>하기</span>
@@ -290,16 +335,16 @@ function PostDetail() {
               <hr className="PD-info_line"></hr>
 
               {/* 입찰 내역 */}
-              <div className={bidListState === 0 ? "PD-bid_list_wrap_closed" : "PD-bid_list_wrap"}>
+              <div className={!bidListState ? "PD-bid_list_wrap_closed" : "PD-bid_list_wrap"}>
                 <div className="PD-bid_list_title_wrap">
                   <span className="PD-bid_list_title">
                     입찰내역&nbsp;
                     <span>{`(${countBid}건)`}</span>
-                    <img className={bidListState === 0 ? "PD-arrow_icon_closed" : "PD-arrow_icon"}
+                    <img className={!bidListState ? "PD-arrow_icon_closed" : "PD-arrow_icon"}
                       alt="입찰내역" src={arrow_icon_brown} onClick={bidListOut}></img>
                   </span>
                 </div>
-                { bidListState === 1 &&
+                { bidListState &&
                   <ul className="PD-bid_list">
                     {bidListMap}
                   </ul>
@@ -307,7 +352,8 @@ function PostDetail() {
               </div>
 
               <hr className="PD-info_line"></hr>
-
+              </>
+            }
               {/* 하자 정보 */}
               <div className="PD-deffect_wrap">
                 <div className="PD-deffect_title_wrap">
@@ -319,24 +365,35 @@ function PostDetail() {
                     <div className="PD-deffect_content">{prodInfo.deffect_text}</div>
                     {prodInfo.deffect_image1 !== null &&
                       <div className="PD-deffect_image_wrap">
-                        { <img className="PD-deffect_image"
-                            alt="하자이미지1" src={`/images/${prodInfo.deffect_image1}`} onClick={imageView(1)}></img>
-                        }
+                        <img className="PD-deffect_image"
+                          alt="하자이미지1" src={`/images/${prodInfo.deffect_image1}`} onClick={() =>{imageViewChange(1)}}></img>
                         { prodInfo.deffect_image2 === null ? <span className="PD-noImage">no<br/>image</span>
                           : <img className="PD-deffect_image"
-                              alt="하자이미지2" src={`/images/${prodInfo.deffect_image2}`} onClick={imageView(2)}></img>
+                              alt="하자이미지2" src={`/images/${prodInfo.deffect_image2}`} onClick={() =>{imageViewChange(2)}}></img>
                         }
                         { prodInfo.deffect_image3 === null ? <span className="PD-noImage">no<br/>image</span>
                           : <img className="PD-deffect_image"
-                              alt="하자이미지3" src={`/images/${prodInfo.deffect_image2}`} onClick={imageView(3)}></img>
+                              alt="하자이미지3" src={`/images/${prodInfo.deffect_image2}`} onClick={() =>{imageViewChange(3)}}></img>
+                        }
+                      </div>
+                    }
+                    { imageView !== 0 &&
+                      <div className="PD-deffect_image_modal_wrap" onClick={() =>{imageViewChange(4)}}>
+                        { imageView === 1 ?
+                          <img className="PD-deffect_image_modal"
+                            alt="하자이미지1" src={`/images/${prodInfo.deffect_image1}`} onClick={() =>{imageViewChange(1)}}></img>
+                          : imageView === 2 ?
+                          <img className="PD-deffect_image_modal"
+                            alt="하자이미지2" src={`/images/${prodInfo.deffect_image2}`} onClick={() =>{imageViewChange(2)}}></img>
+                          : imageView === 3 &&
+                          <img className="PD-deffect_image_modal"
+                            alt="하자이미지3" src={`/images/${prodInfo.deffect_image2}`} onClick={() =>{imageViewChange(3)}}></img>
                         }
                       </div>
                     }
                   </>
                 }
               </div>
-              </>
-            }
           </span>
       </div>
 
