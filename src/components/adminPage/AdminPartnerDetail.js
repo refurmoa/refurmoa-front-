6c6d355ae3f3cf35146e483123153af8306194e0
chj <sajun28@naver.com>
2023-05-18 09:17:07 +0900

    관리자 페이지 제휴 목록

--- src/components/adminPage/AdminPartnerDetail.js
+++ src/components/adminPage/AdminPartnerDetail.js
@@ -2,21 +2,36 @@ import "./AdminPartnerDetail.css";
 import { AdminPartnerDetailList } from "./AdminPartnerDetailList";
 import partnerdata from "./AdminPartnerDetail.json";
 import { useState } from "react";
+import Modal from "react-modal";
+import UpdatePartner from "./AdminPartnerDetailUpdate";
 import axios from "axios";
+import { productList } from "../prodPost/PostWrite/ProdcutList";
 
 const AdminPartnerDetail = () => {
   const APDpush = () => {
-    axios.get("/api/APDpush");
+    // axios
+    //   .post("/api/APDpush", {
+    //     com_name: com_name,
+    //   })
+    //   .then((res) => {
+    //     postUserData();
+    //   });
   };
 
-  const APDupdate = () => {};
-
-  const APDend = () => {};
+  const APDend = () => {
+    // axios
+    //   .post("/api/APDend", {
+    //     com_name: com_name,
+    //   })
+    //   .then((res) => {
+    //     postUserData();
+    //   });
+  };
 
   // const [partnerdata, setPartnerdata] = useState();
-  const postUserData = () => {
+  const getUserData = () => {
     // axios
-    //   .get(`/api/post/PartnerData`{
+    //   .get(`/api/get/PartnerData`{
     //   name : name
     // })
     //   .then((res) => {
@@ -29,10 +44,21 @@ const AdminPartnerDetail = () => {
     // setPartnerdata(data);
   };
 
+  const [Partner_popup, setPartnerPopup] = useState(false);
+  const [Partner_modal, setPartnerModal] = useState(false);
+  const Partner_ChangePopUP = () => {
+    setPartnerPopup(true);
+    setPartnerModal(true);
+  };
+  const Partner_close_modal = () => {
+    setPartnerPopup(false);
+    setPartnerModal(false);
+  };
+
   return (
     <div className="APDAll">
-      <left className="APDLeftAll">
-        <info className="APDLeftInfo">
+      <div className="APDLeftAll">
+        <div className="APDLeftInfo">
           <div>{partnerdata.com_name}</div>
           <div>연락처</div>
           <div>{partnerdata.com_phone}</div>
@@ -45,7 +71,7 @@ const AdminPartnerDetail = () => {
           </div>
           <div>대표명</div>
           <div>{partnerdata.com_ceo_name}</div>
-        </info>
+        </div>
         <div className="APDLeftButton">
           {partnerdata.com_status === 0 ? (
             <div>
@@ -56,7 +82,47 @@ const AdminPartnerDetail = () => {
           ) : partnerdata.com_status === 1 ? (
             <div>
               <div>
-                <input type="button" value="정보 수정" onClick={APDupdate} />
+                <input
+                  type="button"
+                  value="정보 수정"
+                  onClick={Partner_ChangePopUP}
+                />
+                <Modal
+                  style={{
+                    overlay: {
+                      position: "fixed",
+                      backgroundColor: "rgba(0, 0, 0, 0.75)",
+                    },
+                    content: {
+                      position: "absolute",
+                      top: "18%",
+                      width: "1050px",
+                      height: "735px",
+                      left: "40px",
+                      right: "40px",
+                      bottom: "40px",
+                      border: "1px solid #ccc",
+                      background: "#fff",
+                      overflow: "auto",
+                      WebkitOverflowScrolling: "touch",
+                      borderRadius: "15px",
+                      outline: "none",
+                      padding: "30px",
+                    },
+                  }}
+                  isOpen={Partner_modal}
+                >
+                  <div className="close_modal">
+                    <button onClick={Partner_close_modal}>
+                      <b>X</b>
+                    </button>
+                  </div>
+                  <div>
+                    {Partner_popup && (
+                      <UpdatePartner getUserData={getUserData} />
+                    )}
+                  </div>
+                </Modal>
               </div>
               <div>
                 <input type="button" value="제휴 종료" onClick={APDend} />
@@ -66,10 +132,10 @@ const AdminPartnerDetail = () => {
             ""
           )}
         </div>
-      </left>
-      <right className="APDRightAll">
+      </div>
+      <div className="APDRightAll">
         <AdminPartnerDetailList />
-      </right>
+      </div>
     </div>
   );
 };
