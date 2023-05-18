e49317c5ba98ea067c5fb8b2d6008fd62122a8dc
Sejin <w_star25@naver.com>
2023-05-18 13:01:51 +0900

    Refactor: 코드 수정

--- src/components/adminPage/AdminNavi.js
+++ src/components/adminPage/AdminNavi.js
@@ -1,6 +1,6 @@
 // 관리자 페이지 메뉴
 
-import { Outlet, Link, NavLink, useNavigate } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import "./AdminNavi.css";
 
@@ -8,9 +8,7 @@ const AdminNavi = () => {
   const navigate = useNavigate();
   useEffect(() => {
     const login_id = window.sessionStorage.getItem("member_id");
-    if (login_id !== "admin") {
-      navigate("/login");
-    }
+    // if (login_id !== "admin") navigate("/login");
   }, []);
 
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
@@ -24,26 +22,26 @@ const AdminNavi = () => {
     <>
         <div className="AN-wrap">
             <div className="AN-nav_wrap">
-                <NavLink className={`AN-nav AN-nav_${linkStyle("/admin/order")}`}
+                <Link className={`AN-nav AN-nav_${linkStyle("/admin/order")}`}
                     to="/admin/order" onClick={() => setCurrentPath("/admin/order")}>
                     주문/배송
                     <span className={`AN-line AN-line_${linkStyle("/admin/order")}`}></span>
-                </NavLink>
-                <NavLink className={`AN-nav AN-nav_${linkStyle("/admin/user")}`}
+                </Link>
+                <Link className={`AN-nav AN-nav_${linkStyle("/admin/user")}`}
                     to="/admin/user" onClick={() => setCurrentPath("/admin/user")}>
                     회원
                     <span className={`AN-line AN-line_${linkStyle("/admin/user")}`}></span>
-                </NavLink>
-                <NavLink className={`AN-nav AN-nav_${linkStyle("/admin/partner")}`}
+                </Link>
+                <Link className={`AN-nav AN-nav_${linkStyle("/admin/partner")}`}
                     to="/admin/partner" onClick={() => setCurrentPath("/admin/partner")}>
                     제휴회사
                     <span className={`AN-line AN-line_${linkStyle("/admin/partner")}`}></span>
-                </NavLink>
-                <NavLink className={`AN-nav AN-nav_${linkStyle("/admin/banner")}`}
+                </Link>
+                <Link className={`AN-nav AN-nav_${linkStyle("/admin/banner")}`}
                     to="/admin/banner" onClick={() => setCurrentPath("/admin/banner")}>
                     배너
                     <span className={`AN-line AN-line_${linkStyle("/admin/banner")}`}></span>
-                </NavLink>
+                </Link>
                 
                 <Link className={`AN-nav AN-nav_${linkStyle("/admin")}`} to="/prod/write">
                     상품등록
