d0e85c8d44feae250b1b0964577eafdd59cc8748
s-woon <sus9763@gmail.com>
2023-05-27 02:15:18 +0900

    Feat: Faq 목록 조회, 페이징기능, response 키 값 수정

--- src/components/cs/FAQ/FAQPost.js
+++ src/components/cs/FAQ/FAQPost.js
@@ -4,20 +4,19 @@ import arrow from "../../../images/arrow_icon_brown-240.png";
 import "./FAQ.css";
 import userEvent from "@testing-library/user-event";
 import { Link, useLocation } from "react-router-dom";
-const FAQPOST = (props) => {
-  const item = props.item;
-  const logId = props.logId;
+const FAQPOST = ({logId, item}) => {
   const [cate, setCate] = useState("");
   const [mode, setMode] = useState(true);
+
   useEffect(() => {
-    console.log(item.FAQ_cate);
-    if (item.FAQ_cate === 1) setCate("주문/결제");
-    else if (item.FAQ_cate === 2) setCate("배송");
-    else if (item.FAQ_cate === 3) setCate("취소/환불/교환");
-    else if (item.FAQ_cate === 4) setCate("회원");
-    else if (item.FAQ_cate === 5) setCate("경매/낙찰");
-    else if (item.FAQ_cate === 6) setCate("기타");
-  }, []);
+    setMode(true);
+    if (item.faq_cate === 1) setCate("주문/결제");
+    else if (item.faq_cate === 2) setCate("배송");
+    else if (item.faq_cate === 3) setCate("취소/환불/교환");
+    else if (item.faq_cate === 4) setCate("회원");
+    else if (item.faq_cate === 5) setCate("경매/낙찰");
+    else if (item.faq_cate === 6) setCate("기타");
+  }, [item]);
 
   return (
     <>
@@ -26,40 +25,28 @@ const FAQPOST = (props) => {
           {mode && (
             <div className="FAQ_List_post">
               <div className="FAQPostCate">{cate}</div>
-              <div className="FAQAdminTitle" onClick={() => setMode(false)}>
-                {item.title}
-              </div>
+              <div className="FAQAdminTitle" onClick={() => setMode(false)}>{item.faq_title}</div>
               <Link to="/cs/faq/update" state={{ item: item }}>
                 <button className="FAQAdminUpdate">수정</button>
               </Link>
               <button className="FAQAdminDelete">삭제</button>
-              <img
-                className="FAQArrowRotate"
-                src={arrow}
-                onClick={() => setMode(false)}
-              />
+              <img className="FAQArrowRotate" src={arrow} onClick={() => setMode(false)} alt="arrow" />
             </div>
           )}
           {!mode && (
             <>
               <div className="FAQ_List_post">
                 <div className="FAQPostCate">{cate}</div>
-                <div className="FAQAdminTitle" onClick={() => setMode(true)}>
-                  {item.title}
-                </div>
+                <div className="FAQAdminTitle" onClick={() => setMode(true)}>{item.faq_title}</div>
                 <Link to="/cs/faq/update" state={{ item: item }}>
                   <button className="FAQAdminUpdate">수정</button>
                 </Link>
                 <button className="FAQAdminDelete">삭제</button>
-                <img
-                  className="FAQArrow"
-                  src={arrow}
-                  onClick={() => setMode(true)}
-                />
+                <img className="FAQArrow" src={arrow} onClick={() => setMode(true)} alt="arrow" />
               </div>
 
               <div className="FAQ_content">
-                <div className="FAQ_post_content">{item.content}</div>
+                <div className="FAQ_post_content">{item.faq_content}</div>
               </div>
             </>
           )}
@@ -69,31 +56,19 @@ const FAQPOST = (props) => {
           {mode && (
             <div className="FAQ_List_post">
               <div className="FAQPostCate">{cate}</div>
-              <div className="FAQPostTitle" onClick={() => setMode(false)}>
-                {item.title}
-              </div>
-              <img
-                className="FAQArrowRotate"
-                src={arrow}
-                onClick={() => setMode(false)}
-              />
+              <div className="FAQPostTitle" onClick={() => setMode(false)}>{item.faq_title}</div>
+              <img className="FAQArrowRotate" src={arrow} onClick={() => setMode(false)} alt="arrow" />
             </div>
           )}
           {!mode && (
             <>
               <div className="FAQ_List_post">
                 <div className="FAQPostCate">{cate}</div>
-                <div className="FAQPostTitle" onClick={() => setMode(true)}>
-                  {item.title}
-                </div>
-                <img
-                  className="FAQArrow"
-                  src={arrow}
-                  onClick={() => setMode(true)}
-                />
+                <div className="FAQPostTitle" onClick={() => setMode(true)}>{item.faq_title}</div>
+                <img className="FAQArrow" src={arrow} onClick={() => setMode(true)} alt="arrow" />
               </div>
               <div className="FAQ_content">
-                <div className="FAQ_post_content">{item.content}</div>
+                <div className="FAQ_post_content">{item.faq_content}</div>
               </div>
             </>
           )}
