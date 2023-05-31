1ae5a2893775bcec8fcc031d4b7e48119621d61c
chj <sajun28@naver.com>
2023-05-31 23:51:22 +0900

    piechart 추가

--- src/components/adminPage/Admin.js
+++ src/components/adminPage/Admin.js
@@ -1,8 +1,10 @@
 // 관리자 페이지 - 메인
+import { MyResponsivePie } from "./piechart.js";
+import  data  from "./data.json"
 
 const Admin = () => {
     return (
-        <></>
+        <MyResponsivePie data={data} />
     );
 };
 
