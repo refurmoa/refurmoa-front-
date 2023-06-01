0b2eb826ac1cdf65e2b0a7d56f41059cd07ff336
s-woon <sus9763@gmail.com>
2023-06-01 11:12:10 +0900

    Feat: admin 메인페이지 구현(일별 매출, 미답변상품문의, MEMO

---
+++ src/components/adminPage/AdminMemo.js
@@ -0,0 +1,138 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+// 더미데이터
+import adminMemoDummyData from "./AdminMemo.json"
+
+// 이미지
+import memoinputBtn from "../../images/memo_input_btn.png";
+
+const AdminMemo = () => {
+  const [adminMemoData, setAdminMemoData] = useState();
+
+  const activeEnter = (e) => {
+    if(e.key === "Enter") {
+      inputMemo()
+    }
+  }
+  const inputMemo = () => {
+    console.log("입력");
+    // axios.post("/admin/memo/input")
+    // .then((res) => {
+
+    // })
+    // .catch((e) => {
+    //   console.error(e);
+    // })
+  }
+
+
+  const getAdminMemo = () => {
+    // axios.get("/admin/memo")
+    // .then((res) => {
+    //   const { data } = res;
+    //   setAdminMemoData(data.content);
+    // })
+    // .catch((e) => {
+    //   console.error(e);
+    // })
+    setAdminMemoData(adminMemoDummyData);
+
+  }
+
+  useEffect(() => {
+    getAdminMemo();
+  }, [])
+
+  return (
+    <AdminMemeoWrapper>
+      <TitleBox><span>MEMO</span></TitleBox>
+      <MemoListBox>
+        {/* 무한스크롤 */}
+        {adminMemoData?.map((item, index) => (
+          <MemoItem key={index}>{item.memo}</MemoItem>
+        ))}
+      </MemoListBox>
+      <MemoInputBox>
+        <input type="text" onKeyDown={(e) => {activeEnter(e)}} />
+        <img src={memoinputBtn} alt="memoinputbutton" onClick={() => {inputMemo()}}/>
+      </MemoInputBox>
+    </AdminMemeoWrapper>
+  )
+}
+
+export default AdminMemo
+
+const AdminMemeoWrapper = styled.div`
+  margin: 0px;
+  width: 580px;
+  height: 700px;
+`;
+
+const TitleBox = styled.div`
+  width: 100%;
+  height: 40px;
+
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 40px;
+
+  box-sizing: border-box;
+  border-bottom: 1px solid #000000;
+
+  span {
+    margin-left: 15px;
+  }
+`;
+
+const MemoListBox = styled.div`
+  width: 100%;
+  height: 555px;
+
+  background-color: rgba(238, 238, 238, 0.4);
+
+  overflow-y: auto;
+  overflow-x: hidden;
+  ::-webkit-scrollbar {
+    width: 7px;
+  }
+  ::-webkit-scrollbar-thumb {
+    border-radius: 50px;
+    background: #D9D9D9;
+  }
+`;
+
+const MemoItem = styled.div`
+  margin: 10px 15px 10px 15px;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #ffffff;
+`;
+
+const MemoInputBox = styled.div`
+  width: 100%;
+  height: 105px;
+  background-color: rgba(238, 238, 238, 0.4);
+
+  position: relative;
+
+  input {
+    margin: 30px 15px 0px;
+    padding: 10px 60px 10px 10px;
+    width: 550px;
+    height: 60px;
+    box-sizing: border-box;
+    font-size: 20px;
+  }
+  img {
+    width: 30px;
+    height: 30px;
+
+    cursor: pointer;
+
+    position: absolute;
+    right: 25px;
+    bottom: 30px;
+  }
+`;
---
+++ src/components/adminPage/UnAnswered.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import axios from 'axios';
+import { useInView } from 'react-intersection-observer';
+
+// 더미데이터
+import unAnsweredWritings from "./UnAnswered.json";
+import { InView } from 'react-intersection-observer';
+import { useNavigate } from 'react-router-dom';
+
+
+const UnAnswered = () => {
+  const [ref, inView] = useInView();
+  const navigate = useNavigate();
+  const [unAnsweredData, setUnAnsweredData] = useState();
+
+
+  const getUnAnsweredData = () => {
+    // axios.get("/admin/unanswered")
+    // .then((res) => {
+    //   const { data } = res;
+    //   setUnAnsweredData(data.content);
+    // })
+    // .catch((e) => {
+    //   console.error(e);
+    // })
+    setUnAnsweredData(unAnsweredWritings);
+  }
+
+  useEffect(() => {
+    getUnAnsweredData();
+  }, [])
+
+  return (
+    <UnAnsweredWrapper>
+      <TitleBox><span>미답변 상품 문의</span></TitleBox>
+      <WritingsListBox>
+        {unAnsweredData?.map((item, index) => (
+          <WritingItem key={index}>
+            <WritingTitle onClick={() => {navigate(`/post/detail/${item.board_num}`)}}><span>{item.title}</span></WritingTitle>
+            <WritingTime><span>{item.date}</span></WritingTime>
+          </WritingItem>
+        ))}
+        
+      </WritingsListBox>
+    </UnAnsweredWrapper>
+  )
+}
+
+export default UnAnswered
+
+const UnAnsweredWrapper = styled.div`
+  margin: 0px;
+  width: 580px;
+  height: 700px;
+`;
+
+const TitleBox = styled.div`
+  width: 100%;
+  height: 40px;
+
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 40px;
+
+  box-sizing: border-box;
+  border-bottom: 1px solid #000000;
+
+  span {
+    margin-left: 15px;
+  }
+`;
+
+const WritingsListBox = styled.div`
+  width: 100%;
+  height: 660px;
+
+  overflow-y: auto;
+  overflow-x: hidden;
+  ::-webkit-scrollbar {
+    width: 7px;
+  }
+  ::-webkit-scrollbar-thumb {
+    border-radius: 50px;
+    background: #D9D9D9;
+  }
+`;
+
+const WritingItem = styled.div`
+  width: 580px;
+  height: 60px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  box-sizing: border-box;
+  border-bottom: 2px solid rgba(185, 168, 154, 0.3);
+  
+`;
+
+const WritingTitle = styled.div`
+  margin: 0px;
+  width: 390px;
+  
+  span {  
+    margin: 0px 0px 0px 15px;
+    font-weight: 400;
+    font-size: 20px;
+    color: #000000;
+    cursor: pointer;
+  }
+`;
+
+const WritingTime = styled.div`
+  margin: 0px;
+
+  span {
+    margin: 0px 15px 0px 0px;
+    font-weight: 400;
+    font-size: 18px;
+    color: #000000;
+  }
+`;
