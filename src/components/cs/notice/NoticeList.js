fd84f82697a6bc68e9c5aa8d45cecd9aa06e15f0
ijh4u <ijh4u1@naver.com>
2023-06-05 14:08:02 +0900

    FIX: 등록일 역순 출력

--- src/components/cs/notice/NoticeList.js
+++ src/components/cs/notice/NoticeList.js
@@ -19,7 +19,7 @@ const NoticeList = () => {
   // 문의 글 목록 조회
   const NoticeList = () => {
     axios
-      .get(`/cs/notice?page=${currentPage - 1}&size=10`)
+      .get(`/cs/notice?page=${currentPage - 1}&size=10&sort=notiNum,desc`)
       .then((res) => {
         setDataList(res.data.content);
         setTotalPage(res.data.totalPages);
