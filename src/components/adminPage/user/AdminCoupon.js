f54edf1e577c708a9c4459171f7ce919e388461e
ijh4u <ijh4u1@naver.com>
2023-06-07 03:36:30 +0900

    FEAT: 쿠폰 등록

--- src/components/adminPage/user/AdminCoupon.js
+++ src/components/adminPage/user/AdminCoupon.js
@@ -2,98 +2,72 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./CouponList.css";
-import cancel from "../../images/cancel.png";
+import "./AdminCoupon.css"
+import cancel from "../../../images/cancel.png";
 import moment from "moment";
-const CouponList = (props) => {
+const AdminCoupon = (props) => {
     const id=props.id;
-    const [totalPage, setTotalPage] = useState(1); // 총 페이지 수
-    const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
-    const [couponList,setCouponList]=useState([]);//쿠폰 리스트
-    const state =props.state;
-    const setPay=props.setPay;
-    const pay=props.pay;
-  // 쿠폰 찾기
-    useEffect(()=> {
+    const[coupon,setCoupon]=useState("");
+    const[sale_price,setSale_price]=useState();
+    const[issue_date,setIssue_date]=useState();
+    const[valid_date,setValid_date]=useState();
+    const addComma = (price) => {
+      let returnString = price?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      return returnString;
+    };
+    const Coupon_regi=()=>{
+      if(coupon===""||sale_price===null||issue_date===null||valid_date===null){
+        alert("모든 정보가 입력되지 않았습니다.")
+        return false;
+      }
+      else{
         axios
-        .get(`/pay/coupon?id=${props.id}&page=${currentPage-1}&size=10`)
+        .post(`/admin/user/detail/coupon/insert`,{
+            memberId:id,
+            couponName:coupon,
+            salePrice:sale_price,
+            issueDate:issue_date,
+            validDate:valid_date,
+          })
         .then((res) => {
-            if(res.data.content.length===0){
-              alert("쿠폰이 존재하지 않습니다.")
-              props.setModal(false);
-            }else{
-              setCouponList(res.data.content);
-              setTotalPage(res.data.totalPages);
-            }
+          alert("쿠폰 등록이 완료되었습니다.");
+          props.setCouponModal(false);
         })
         .catch((e) => {
-            console.error(e);
+          alert("쿠폰 등록에 실패했습니다.")
+          console.error(e);
         });
-    },[currentPage])
-    //pay 정보 업데이트
-    const setCouponInfo=(item)=>{
-        setPay({ 
-            ...pay,
-            coupon_num:item.coupon_num,
-            coupon_price:item.sale_price,
-        }); 
-        props.setModal(false);//모달 종료
+        }
     }
+
   return (
-    <div className="Coupon_find_wrap">
-      <table className="Coupon_table">
-            <thead>
-              <tr>
-                <th>쿠폰명</th>
-                <th>쿠폰 할인가</th>
-                <th>쿠폰 만료일</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {couponList.map((item, index) => {//쿠폰 정보
-                return (
-                  <tr key={index} onClick={()=>{state===1&&setCouponInfo(item)}}>
-                    <td>{item.coupon_name}</td>
-                    <td>{item.sale_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원</td>
-                    <td>{moment(item.valid_date).format("YYYY-MM-DD")}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-
-      {/* 페이지 출력 */}
-      {totalPage > 1 && (
-        <div className="NL-page">
-          {currentPage === 1 ? (
-            <span className="NL-page_prev_gray">&lt;</span>
-          ) : (
-            <span
-              className="NL-page_prev"
-              onClick={() => setCurrentPage(currentPage - 1)}
-            >
-              &lt;
-            </span>
-          )}
-          <span className="NL-page_now">{currentPage}</span>
-          &nbsp;&nbsp;/&nbsp;&nbsp;
-          <span className="NL-page_total">{totalPage}</span>
-          {currentPage === totalPage ? (
-            <span className="NL-page_next_gray">&gt;</span>
-          ) : (
-            <span
-              className="NL-page_next"
-              onClick={() => setCurrentPage(currentPage + 1)}
-            >
-              &gt;
-            </span>
-          )}
-        </div>
-      )}
-        <img className="Sign_modal_close" alt="창 닫기" src={cancel} onClick={() => {props.setModal(false);}} />
+      <div className="Coupon_regi_wrap">
+       <div className="Coupon_regi_Title" >쿠폰 등록</div>
+       <div className="Coupon_regi_content">
+        <div className="Coupon_name" htmlFor="name">쿠폰 이름</div>
+        <input className="Coupon_name_input" name="name" maxLength={20}
+        type="text" value={coupon} onChange={(e)=>setCoupon(e.target.value)} required />
+      </div> 
+      <div className="Coupon_regi_content">
+        <div className="Coupon_name" htmlFor="name">할인 가격</div>
+        <input className="Coupon_price_input" 
+          name="name" type="number" value={sale_price}  onChange={(e)=>setSale_price(e.target.value)}required />
+        <div className="Coupon_won">원</div>
+      </div>
+      <div className="Coupon_regi_content">
+        <div className="Coupon_name" htmlFor="name">쿠폰 발행일</div>
+        <input className="Coupon_name_input" name="name" 
+        type="date" value={issue_date} onChange={(e)=>setIssue_date(e.target.value)} required />
+      </div> 
+      <div className="Coupon_regi_content">
+        <div className="Coupon_name" htmlFor="name">쿠폰 만료일</div>
+        <input className="Coupon_name_input" name="name" 
+        type="date" value={valid_date} onChange={(e)=>setValid_date(e.target.value)} required />
+      </div> 
+      <div className="Coupon_regi_button" onClick={()=>Coupon_regi()}>등록</div>
+      <img className="Sign_modal_close" alt="창 닫기" src={cancel} onClick={() => {props.setCouponModal(false);}} />
      </div>  
   );
 };
 
-export default CouponList;
+export default AdminCoupon;
